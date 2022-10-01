ac6539a08c74cb25bda1c33d27a24eeef3874956
Sam <sambisselele@gmail.com>
2022-10-01 17:24:18 +0100

    adding route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Term from "./pages/Term";
+import Policy from "./pages/Policy";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/term" element={<Term />} />
+          <Route path="policy" element={<Policy />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
